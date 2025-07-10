601d59df566356933861dd82be5403a470a0fca8
Shormi Ghosh <quanta5419@gmail.com>
2025-07-11 03:05:47 +0600

    complete without responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.html",
-    "./src/**/*.{html,js,ts,jsx,tsx}"],
+    "*.html"],
   theme: {
     extend: {},
   },
